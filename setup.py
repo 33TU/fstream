b92afae2133a191a998e3a4642487651cf1ceb36
33TU <89095694+33TU@users.noreply.github.com>
2021-11-06 18:32:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ project_urls = {
 
 setup(
     name="fstream",
-    version='0.0.5',
+    version='0.0.6',
     description="Faster async streams for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
